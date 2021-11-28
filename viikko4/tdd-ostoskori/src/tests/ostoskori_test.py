34270b33b5880f04c59eb32404789e38b83f5493
taapp <julius.nieminen@helsinki.fi>
2021-11-28 13:43:33 +0200

    Ostoskori-luokan testeihin lisätty tapaus, että kahden saman tuotteen lisäämisen jälkeen ostoskorin hinta on sama kuin kaksi kertaa tuotteen hinta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -40,3 +40,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_hinta_sama_kuin_tuotteen_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
+
