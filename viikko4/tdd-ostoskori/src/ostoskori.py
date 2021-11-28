5e3a7cdb352b06a4b87b308a62b7badcf17ab18f
taapp <julius.nieminen@helsinki.fi>
2021-11-28 14:12:18 +0200

    Ostoskori-luokkaa laajennettu niin, että yhden tuotteen lisäämisen jälkeen ostoskorin ostokset-metodi palauttaa listan, jonka pituus on yksi, ja tälle on tehty testi. Lisäksi on muuten refaktoroitu Ostoskori-luokkaa.

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,23 +4,21 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self.n_tavaroita = 0
-        self.hinta_luku = 0
+        self.tavarat = []
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        return self.n_tavaroita
+        return len(self.tavarat)
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
-        return self.hinta_luku
+        return sum([tavara.hinta() for tavara in self.tavarat])
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        self.n_tavaroita += 1
-        self.hinta_luku = self.hinta_luku + lisattava.hinta()
+        self.tavarat.append(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -31,6 +29,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        return self.tavarat
