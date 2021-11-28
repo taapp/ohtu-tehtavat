b14934d0da6a0a17851f5ee8fec689309ea68f81
taapp <julius.nieminen@helsinki.fi>
2021-11-28 13:32:54 +0200

    Ostoskori-luokan testeihin lisätty tapaus, että kahden tuotteen lisäämisen jälkeen tavaroiden määrä ostoskorissa on kaksi

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        piima = Tuote("Piimä", 4)
+        self.kori.lisaa_tuote(piima)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
