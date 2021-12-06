b802738ec80e7262f96f47e80506ed05238e44c5
taapp <julius.nieminen@helsinki.fi>
2021-12-06 18:38:16 +0200

    Refaktoroitu TennisGame-luokkaa

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -11,47 +11,32 @@ class TennisGame:
         else:
             self.m_score2 = self.m_score2 + 1
 
+    def score_component_to_str(self, score):
+        score_component_to_str_dict = {
+            0: 'Love',
+            1: 'Fifteen',
+            2: 'Thirty',
+            3: 'Forty'
+        }
+        return score_component_to_str_dict[score]
+
     def get_score(self):
-        score = ""
-        temp_score = 0
-
         if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
+            return self.get_score_even()
         elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+            return self.get_score_not_even_at_least_four()
+        return self.get_score_not_even_less_than_four()
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+    def get_score_not_even_less_than_four(self):
+        return f"{self.score_component_to_str(self.m_score1)}-{self.score_component_to_str(self.m_score2)}"
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+    def get_score_not_even_at_least_four(self):
+        difference_score = self.m_score1 - self. m_score2
+        status_str = 'Advantage player' if abs(difference_score)==1 else "Win for player"
+        player_str = '1' if difference_score > 0 else "2"
+        return status_str + player_str
+    
+    def get_score_even(self):
+        if self.m_score1 > 3:
+            return "Deuce"
+        return f"{self.score_component_to_str(self.m_score1)}-All"
