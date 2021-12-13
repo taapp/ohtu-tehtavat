a2e60335e46706a16705e6ecccd0f92129d938f1
taapp <julius.nieminen@helsinki.fi>
2021-12-13 20:54:19 +0200

    Lisätty query-language-projektiin All, Not ja HasFewerThan-luokat

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,16 +1,24 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan#, Or, QueryBuilder
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    #matcher = And(
+    #    HasAtLeast(5, "goals"),
+    #    HasAtLeast(5, "assists"),
+    #    PlaysIn("PHI")
+    #)
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
+       Not(HasAtLeast(1, "goals")),
+       PlaysIn("NYR")
+    )
+    matcher = And(
+       HasFewerThan(1, "goals"),
+       PlaysIn("NYR")
     )
 
     for player in stats.matches(matcher):
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -25,3 +25,28 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+class All:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+    
+    def matches(self, player):    
+        return True
+
+class Not:
+    def __init__(self,matcher):
+        self._matcher = matcher
+
+    def matches(self, player):
+        return not self._matcher.matches(player)
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
