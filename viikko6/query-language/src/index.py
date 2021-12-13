869eba0da078d2749faef6a8443128873875e7df
taapp <julius.nieminen@helsinki.fi>
2021-12-13 20:57:48 +0200

    Lisätty query-language-projektiin Or-luokka

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan#, Or, QueryBuilder
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, Or
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
@@ -20,6 +20,21 @@ def main():
        HasFewerThan(1, "goals"),
        PlaysIn("NYR")
     )
+    
+    #matcher = Or(
+    #    HasAtLeast(30, "goals"),
+    #    HasAtLeast(50, "assists")
+    #)
+
+    matcher = And(
+        HasAtLeast(40, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("NYI"),
+            PlaysIn("BOS")
+        )
+    )
+
 
     for player in stats.matches(matcher):
         print(player)
